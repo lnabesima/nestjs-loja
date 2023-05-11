68d13ca8208efa1d53b033bda509fa34af0e188d
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-05-11 11:55:44 -0300

    feat: create DTO and validation when POSTing on '/usuarios' endpoint

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
+import { CriaUsuarioDTO } from './dto/usuario.dto';
 
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   @Post()
-  async criaUsuario(@Body() dadosDoUsuario): Promise<any> {
+  async criaUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO): Promise<any> {
     await this.usuarioRepository.salvar(dadosDoUsuario);
     return dadosDoUsuario;
   }
